8b88d3c159378ad7b5966a2ed6add60327f32300
JingChen <jingchenjc2019@gmail.com>
2019-09-17 23:00:09 -0500

    Components set up.

---
+++ google-book-finder/src/BookCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class BookCard extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <p>Book Card here.</p>
+            </div>
+        )
+    }
+}
---
+++ google-book-finder/src/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class SearchBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Search Bar here</h2>
+            </div>
+        )
+    }
+}
