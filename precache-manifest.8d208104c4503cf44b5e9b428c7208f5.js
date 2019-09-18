32b434df55e6de978e2dc8a65093cb480ec35935
JingChen <jingchenjc2019@gmail.com>
2019-09-17 23:20:35 -0500

    Updates

---
+++ precache-manifest.8d208104c4503cf44b5e9b428c7208f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780a975d32608f550e57296616447431",
+    "url": "/BookFinder/index.html"
+  },
+  {
+    "revision": "5df9a391813485728f63",
+    "url": "/BookFinder/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "93f5954d55fe0ae0d692",
+    "url": "/BookFinder/static/js/2.beb3ab46.chunk.js"
+  },
+  {
+    "revision": "5df9a391813485728f63",
+    "url": "/BookFinder/static/js/main.37a70b2e.chunk.js"
+  },
+  {
+    "revision": "c3874d62e03248ae5c52",
+    "url": "/BookFinder/static/js/runtime~main.9b68dac9.js"
+  }
+]);
